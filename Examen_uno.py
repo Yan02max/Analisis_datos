953c9d0fa58c20b6183ea64fb0f522f2775de06f
Yan02max <dauri02041116@gmail.com>
2026-02-19 12:20:52 -0400

    Dashboard, Pandas, Numpy

---
+++ Examen_uno.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+data = {
+    "Producto": ["Tablet", "Smartphone", "Audifonos", "Smartwatch", "Camara"],
+    "Ventas": [800, 2000, 600, 900, 500],
+    "Costo": [500, 1400, 300, 700, 350]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+df["Ganancias"] = df["Ventas"] - df["Costo"]
+
+
+df["Margen"] = (df["Ganancias"] / df["Ventas"]) * 100
+df["Participacion %"] = (df["Ventas"] / df["Ventas"].sum()) * 100
+df["Clasificacion"] = np.where(df["Margen"] >= 40, "Alta",
+                    np.where(df["Margen"] <= 25, "Media", "Baja"))
+print("\n",df)
+mayor_ganacias = df["Ganancias"].max()
+mayor_margen = df["Margen"].max()
+producto_riesgo = df["Participacion %"].min()
+
+# Impresiones
+print(f"\nProducto con mayor ganancia: {mayor_ganacias}")
+print(f"Producto con mayor margen: {mayor_margen}")
+print(f"Producto que más riesgo genera por concentración: {producto_riesgo}\n")
+
